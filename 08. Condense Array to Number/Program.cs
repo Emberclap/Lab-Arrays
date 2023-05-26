169766cb9eb6aad5bb7c6158f9e3eb028db6fed3
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-05-27 02:30:56 +0300

    Add project files.

---
+++ 08. Condense Array to Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            
+
+            while (arrayOfIntegers.Length > 0)
+            {
+
+                if (arrayOfIntegers.Length == 1)
+                {
+                    Console.WriteLine(arrayOfIntegers[0]);
+                    break;
+                }
+                int[] condensed = new int[arrayOfIntegers.Length - 1];
+                
+                for (int i = 0; i < arrayOfIntegers.Length - 1; i++)
+                {
+                    condensed[i] = arrayOfIntegers[i] + arrayOfIntegers[i + 1];
+                    arrayOfIntegers[i] = condensed[i];
+                }
+                arrayOfIntegers = condensed;
+                
+            }
+            
+
+        }
+    }
+}
---
+++ 7. Equal Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _7._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arrayTwo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool notEqual = false;
+            int sum = 0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+               if (arrayOne[i] != arrayTwo[i])
+               {
+                   Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                   notEqual = true;
+                   break;
+               }
+               sum += arrayOne[i];
+            }
+            if (notEqual == false)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
